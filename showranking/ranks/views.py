b9b072b66f2c089da81074e76b0bee5d3b104f31
Michał Rostecki <vador@user.mydevil.net>
2013-05-14 22:07:15 +0200

    view and url

--- showranking/ranks/views.py
+++ showranking/ranks/views.py
@@ -14,7 +14,12 @@ def save_rank(request):
             pass
         else:
             points = int(points)
-            rank = Ranks(user_id=user_id, username=username, points=points)
+            rank = Ranks.objects.get(user_id=user_id)
+            if rank:
+                rank.username = user_id
+                rank.points = points
+            else:
+                rank = Ranks(user_id=user_id, username=username, points=points)
             rank.save()
             success = True
     response = {'success': success}
