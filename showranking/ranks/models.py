9125177c26772346cbbafb1d0a02d0cce422774a
Michał Rostecki <vador@user.mydevil.net>
2013-05-14 21:44:08 +0200

    view

--- showranking/ranks/models.py
+++ showranking/ranks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Ranks(models.Model):
+    user_id = models.CharField(max_length=255, unique=True)
     username = models.CharField(max_length=255)
-    ranking = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
--- showranking/ranks/views.py
+++ showranking/ranks/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.utils import simplejson
+from ranks.models import Ranks
+
+def save_rank(request):
+    success = False
+    if request.method == 'POST':
+        json_request = simplejson.loads(request.raw_post_data)
+        try:
+            user_id = request.POST['id']
+            username = request.POST['username']
+            points = request.POST['points']
+        except KeyError:
+            pass
+        else:
+            points = int(points)
+            rank = Ranks(user_id=user_id, username=username, points=points)
+            rank.save()
+            success = True
+    response = {'success': success}
+    return HttpResponse(simplejson.dumps(response), mimetype='application/json')
